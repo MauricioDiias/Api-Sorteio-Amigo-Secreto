55121ed6996dcd1189d55daeea602d75efe2feb6
MauricioDiias <mauriciod664@gmail.com>
2022-12-15 10:15:33 -0300

    reconhecimento de codigo

--- node/api/src/bancoDeDados.js
+++ node/api/src/bancoDeDados.js
@@ -1,29 +1,28 @@
 let nomesRecebidos = [
-    'Ariane',
-    'Lyndi',
-    'Jamile',
-    'Erisson',
-    
+    'a',
+    'b',
+    'c',
+    'd',
+
 ]
-embaralhar(nomesRecebidos)
 function embaralhar(arr) {
     arr.forEach((element, index) => {
         let aux = Math.floor(Math.random() * (index + 1));
-            [arr[index], arr[aux]] = [arr[aux], arr[index]];
-        
+        [arr[index], arr[aux]] = [arr[aux], arr[index]];
+
     });
 }
 
-function sortear(){
+function sortear() {
     embaralhar(nomesRecebidos)
     let sorteados = []
-    nomesRecebidos.forEach((element,index) => {
+    nomesRecebidos.forEach((element, index) => {
         sorteados.push([
             nomesRecebidos[index],
-            nomesRecebidos[index!= nomesRecebidos.length - 1 ? +1 : 0]
+            nomesRecebidos[index != nomesRecebidos.length - 1 ? index + 1 : 0]
         ])
-        console.log(sorteados)
     })
+    console.log(sorteados)
 }
 sortear()
 

240e27d994c9fa9ab0a49909ab7581cf02f3cac7
cypirando <fabiocypriano@gmail.com>
2022-12-16 10:06:59 -0300

    atualização

--- node/api/src/bancoDeDados.js
+++ node/api/src/bancoDeDados.js
@@ -1,42 +1,43 @@
 const sequence = {
-    _id: 1,
-    get id() { return this._id++ }
-}
+  _id: 1,
+  get id() {
+    return this._id++;
+  },
+};
 
-let nomesRecebidos = []
+let nomesRecebidos = [];
 
 function salvarNomes(nome) {
-    if (!nome.id) nome.id = sequence.id
-    nomesRecebidos[nome.id] = nome
-    return nome
+  if (!nome.id) nome.id = sequence.id;
+  nomesRecebidos[nome.id] = nome;
+  return nome;
 }
 
 function getNome(id) {
-    return nomesRecebidos[id] || []
+  return nomesRecebidos[id] || [];
 }
 
 function getNomes() {
-    return nomesRecebidos
+  return nomesRecebidos;
 }
 // console.log(nomesRecebidos)
 function embaralhar(arr) {
-    arr.forEach((element, index) => {
-        let aux = Math.floor(Math.random() * (index + 1));
-        [arr[index], arr[aux]] = [arr[aux], arr[index]];
-
-    });
+  arr.forEach((element, index) => {
+    let aux = Math.floor(Math.random() * (index + 1));
+    [arr[index], arr[aux]] = [arr[aux], arr[index]];
+  });
 }
 
 function sortear(teste) {
-    embaralhar(teste)
-    let sorteados = []
-    teste.map((element, index) => {
-        sorteados.push([
-            teste[index],
-            teste[index != teste.length - 1 ? index + 1 : 0]
-        ])
-    })
-    console.log(sorteados)
+  embaralhar(teste);
+  let sorteados = [];
+  teste.map((element, index) => {
+    sorteados.push([
+      teste[index],
+      teste[index != teste.length - 1 ? index + 1 : 0],
+    ]);
+  });
+  console.log(sorteados);
 }
 
-module.exports = {salvarNomes, getNome, getNomes, sortear}
+module.exports = { salvarNomes, getNome, getNomes, sortear };
--- node/api/src/servidor.js
+++ node/api/src/servidor.js
@@ -1,9 +1,11 @@
 const porta = 3003;
 let teste;
+
 const express = require("express");
 const app = express();
 const bancoDeDados = require("./bancoDeDados");
 // const bodyParser = require('body-parser')
+
 app.use(express.json());
 
 app.post("/sort", (req, res, next) => {
@@ -21,6 +23,7 @@ app.post("/sort", (req, res, next) => {
   console.log(teste);
   return { status: 200, data: sorteio }; //JSON
 });
+
 app.get("/ola", (req, res, netx) => {
   const fabio = bancoDeDados.sortear(teste);
   res.send(fabio);
@@ -30,9 +33,11 @@ app.get("/ola", (req, res, netx) => {
 app.get("/sortLista", (req, res, next) => {
   res.send(bancoDeDados.getNomes());
 });
+
 app.get("/sortLista/:id", (req, res, next) => {
   res.send(bancoDeDados.getNome(req.params.id));
 });
+
 app.listen(porta, () => {
   console.log(`Servidor executando na porta ${porta}`);
 });

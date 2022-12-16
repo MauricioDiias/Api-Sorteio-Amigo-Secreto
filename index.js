3f84cc80af4a07f699d479d5a287d0ff1998f013
cypirando <fabiocypriano@gmail.com>
2022-12-16 13:54:18 -0300

    Atualização Cors

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const consulta = 'http://localhost:3003/ola'
-const cliente = []
-const texto = document.querySelector('#teste')
-getApi()
-console.log(consulta)
+const consulta = "http://localhost:3003/ola";
+const cliente = [];
+const texto = document.querySelector("#teste");
+getApi();
+console.log(consulta);
 
 async function getApi() {
+  try {
     const res = await fetch(consulta);
     cliente = await res.json();
-  console.log(cliente)
-  
+  } catch (error) {
+    console.log("eror");
   }
-cliente.forEach( (element) =>{
-    texto.innerHTML += (
-        `<div class="fabio">${element}</div>`
-    )
-})
+}
+cliente.forEach((element) => {
+  texto.innerHTML += `<div class="texto">${element}</div>`;
+});

31825b0fb47c8f5215a306e46193fcba00c486aa
cypirando <fabiocypriano@gmail.com>
2022-12-29 11:20:40 -0300

    Refatoring

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Header",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).send({});
+  }
   next();
 });
 //Rotas
--- js/main.js
+++ js/main.js
@@ -1,37 +1,44 @@
 /* -------------ANIMAÇÕES ------*/
-const slidePage = document.querySelector(".slidepage")
-const avancarBtn = document.querySelector(".nextbtn")
-const Voltar = document.querySelector(".prev-1")
-const sortear = document.querySelector(".next-1")
-const prevBtnSec2 = document.querySelector(".prev-2")
-const nextBtnSec2 = document.querySelector(".next-2")
-const progressText = document.querySelectorAll(".step p")
-const progressCheck = document.querySelectorAll(".step .check")
-const bullet = document.querySelectorAll(".step .bullet")
+const slidePage = document.querySelector(".slidepage");
+const avancarBtn = document.querySelector(".nextbtn");
+const Voltar = document.querySelector(".prev-1");
+const sortear = document.querySelector(".next-1");
+const prevBtnSec2 = document.querySelector(".prev-2");
+const nextBtnSec2 = document.querySelector(".next-2");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
 /******************************************** */
 /**ROTA */
 /* -------------------------------- */
-const rota = "http://localhost:5001/v2-sorteio";
+const rota = "http://localhost:5001/v2-sorteio ";
 /* -------------------------------- */
-async function getApi(nomes) {
+async function getApi(listaDeUsuarios) {
   try {
     let header = new Headers({
-      "Content-Type": "application/json",
+      "access-control-allow-origin": "*",
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Methods': '*',
+    "Content-Type": "application/json"
     });
     let request = await fetch(rota, {
       method: "POST",
       headers: header,
-      body: JSON.stringify({ listaDeUsuarios}),
+      body: JSON.stringify({ listaDeUsuarios }),
     });
+    console.log(header)
     request
       .json()
       .then((sorteados) => {
         let printTela = document.getElementById("lista-one");
         printTela.innerHTML = "";
-        printTela.innerHTML +=Object.values(sorteados)
-        .map(
-            (i) =>console.log('i',i)
-              `<div class="caixa-box name-2"><h3>${i.nome}</h3></div>`
+        printTela.innerHTML += Object.values(sorteados)
+          .map(
+            (i) =>
+              console.log(
+                "i",
+                i
+              )`<div class="caixa-box name-2"><h3>${i.nome}</h3></div>`
           )
           .join(" ");
       })
@@ -44,56 +51,55 @@ async function imprimirNaTela() {
   await getApi(participantes);
 }
 /* -------------------------------- */
-let max = 4
-let current = 1
+let max = 4;
+let current = 1;
 
-let nomeDoSorteio = document.getElementById("input-one")
-let printNomeDoSorteio = document.getElementById("nome-sorteio")
+let nomeDoSorteio = document.getElementById("input-one");
+let printNomeDoSorteio = document.getElementById("nome-sorteio");
 
 avancarBtn.addEventListener("click", (e) => {
-  e.preventDefault()
-  slidePage.style.marginLeft = "-25%"
-  bullet[current - 1].classList.add("active")
-  progressCheck[current - 1].classList.add("active")
-  current += 1
+  e.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  current += 1;
 
-  printNomeDoSorteio.innerHTML = nomeDoSorteio.value
-  limparInputOne()
-})
-
+  printNomeDoSorteio.innerHTML = nomeDoSorteio.value;
+  limparInputOne();
+});
 
 sortear.addEventListener("click", (e) => {
-  e.preventDefault()
-  slidePage.style.marginLeft = "-50%"
-  bullet[current - 1].classList.add("active")
-  progressCheck[current - 1].classList.add("active")
-  current += 1
-})
+  e.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  current += 1;
+});
 
 nextBtnSec2.addEventListener("click", (e) => {
-  e.preventDefault()
-  slidePage.style.marginLeft = "-75%"
-  bullet[current - 1].classList.add("active")
-  progressCheck[current - 1].classList.add("active")
-  current += 1
-})
+  e.preventDefault();
+  slidePage.style.marginLeft = "-75%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  current += 1;
+});
 
 Voltar.addEventListener("click", (e) => {
-  e.preventDefault()
+  e.preventDefault();
 
-  slidePage.style.marginLeft = "0%"
-  bullet[current - 2].classList.remove("active")
-  progressCheck[current - 2].classList.remove("active")
-  current -= 1
-})
+  slidePage.style.marginLeft = "0%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  current -= 1;
+});
 
 prevBtnSec2.addEventListener("click", (e) => {
-  e.preventDefault()
-  slidePage.style.marginLeft = "-25%"
-  bullet[current - 2].classList.remove("active")
-  progressCheck[current - 2].classList.remove("active")
-  current -= 1
-})
+  e.preventDefault();
+  slidePage.style.marginLeft = "-25%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  current -= 1;
+});
 /********************************************************* */
 
 /**limpar Inputs */
@@ -127,10 +133,10 @@ let participantes = [];
 let cadastrados = [];
 let linhas = 0;
 
-console.log(participantes)
+console.log(participantes);
 
 btnAdicionar.addEventListener("click", (e) => {
-  e.preventDefault()
+  e.preventDefault();
   let input = document.querySelector("#input-dados").value;
   let inputemail = document.querySelector("#input-email").value;
 
@@ -163,5 +169,9 @@ function renderizar() {
     `
     )
     .join("");
-
 }
+const btnSortear = document.getElementById("btn-sortear");
+btnSortear.addEventListener("click", () => {
+  console.log("oi cliquei");
+  imprimirNaTela()
+});
